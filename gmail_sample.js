accee7640f2615994e0adba595b2ca8235de439f
redcap3000 <redcap3000@gmail.com>
2020-03-09 20:05:04 -0800

    starting on ingest of cash card activity via gmail API

--- gmail_sample.js
+++ gmail_sample.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const readline = require('readline');
 const {google} = require('googleapis');
-
 // If modifying these scopes, delete token.json.
 const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
 // The file token.json stores the user's access and refresh tokens, and is
@@ -13,7 +12,15 @@ const TOKEN_PATH = 'token.json';
 fs.readFile('credentials.json', (err, content) => {
   if (err) return console.log('Error loading client secret file:', err);
   // Authorize a client with credentials, then call the Gmail API.
-  authorize(JSON.parse(content), listLabels);
+  var auth = JSON.parse(content)
+  authorize(auth, listLabels);
+  
+  cb = function(e,r){
+    console.log(e)
+    console.log(r)
+  }
+  console.log(typeof cb)
+  authorize(auth, listMessages)
 });
 
 /**
@@ -88,3 +95,190 @@ function listLabels(auth) {
     }
   });
 }
+
+/*
+function getMessages(auth) {
+  const gmail = google.gmail({version: 'v1', auth});
+  gmail.users.messages.list({
+    userId: 'me',
+  }, (err, res) => {
+    if (err) return console.log('The API returned an error: ' + err);
+    const labels = res.data.labels;
+    if (labels.length) {
+      console.log('Labels:');
+      labels.forEach((label) => {
+        console.log(`- ${label.name}`);
+      });
+    } else {
+      console.log('No labels found.');
+    }
+  });
+}
+
+*/
+
+function listMessages(auth) {
+  // maxResults
+  // pageToken
+  // q (query) "from:someuser@example.com rfc822msgid:<somemsgid@example.com> is:unread"
+  query = "from:cash@square.com"
+        
+  const userId = 'me'
+  const gmail = google.gmail({version: 'v1', auth});
+
+  function getMessage(userId, messageId, callback) {
+    var r = gmail.users.messages.get({
+      'userId': userId,
+      'id': messageId
+    });
+    r.then(callback);
+  }
+
+
+  function msgHandler(r,e){
+    const fieldFilter = [
+      'id',
+      'threadId',
+      'labelIds',
+      'snippet',
+      'historyId',
+      'internalDate',
+      'payload'
+    ]
+    var d = {}
+    //console.log(r.body)
+    for(var k in r.data){
+      if(fieldFilter.indexOf(k) > -1){
+        d[k]=r.data[k]
+      }
+    }
+   // console.log(typeof d.payload)
+    var payloadFilter = [
+      'Subject',
+    //  'Date',
+
+    ]
+    
+    var cashCardPreTerms = {
+
+
+      'buy' : 'You purchased',
+      'sell' : 'You sold',
+      'spend' : 'You spent'
+
+
+    }
+
+    var cashCardPostTerms = {
+      'deposit' : 'depost has been recieved',
+      'depositAvailable' : 'deposit completed',
+      'boostApplied' : 'instantly applied',
+      'paymentDecline' : 'was declined'
+
+    }
+
+    // <term> sent you $200
+    var cashCardMidTerms = {
+      'recieved' : 'sent you',
+
+    }
+    var line = ''
+    d.payload.headers.filter(function(p){
+      var key = p.name
+      if(payloadFilter.indexOf(key) > -1){
+        //console.log(p.value) 
+        line +=p.value +' '       
+      }
+    })
+    //console.log(line)
+    var match = []
+    for(var action in cashCardPreTerms){
+      var term = cashCardPreTerms[action]
+      var term2 = ''
+      var term3 = []
+      match = line.split(term)
+      if(match.length > 1){
+        if(action == 'spend'){
+          console.log(line)
+
+          term2 = line.split(cashCardPostTerms['boostApplied'])
+          // has boost? then split on period to extract
+          term3 = line.split(' at ')
+          var amount = term3[0].split('$')[1]
+
+          var term4 = term3[1].split('. Your ')
+
+          var where = term4[0]
+
+          if(term2.length == 2){
+            // get 'where money was spent... '
+            var boostAmount = term4[1].split(' ')[0]
+            console.log(action + '\t' + amount + '\t' + where + '\t***' + boostAmount)
+            
+          }else{
+            console.log(action + '\t' + amount + '\t' + where )
+            //console.log('Pre\t'+action + '\t' + match[1])
+          
+          }
+        }else{
+
+        }
+      }
+    }
+    //if(match.length === 1){
+    /*
+    for(var action in cashCardPostTerms){
+      var term = cashCardPostTerms[action]
+      match = line.split(term)
+      if(match.length > 1){
+        console.log('Post\t'+action + '\t' + match[1])
+      }
+    }
+    */ 
+    //}
+    // have to get attachment parts
+    /*
+    {
+
+
+      id : <string>,
+      threadId: <string>,
+      labelIds : <array>,
+      snippet : <blob>,
+      historyId : <string/int>,
+      internalDate : <string/int - unix timestamp>
+
+
+    }*/
+
+  }
+  var getPageOfMessages = function(r, result) {
+    r.then(function(resp) {
+      result = result.concat(resp.data.messages);
+      var nextPageToken = resp.nextPageToken;
+      if (typeof nextPageToken != 'undefined' && nextPageToken) {
+        console.log("getting next page")
+        request = gmail.users.messages.list({
+          'userId': userId,
+          'pageToken': nextPageToken,
+          'q': query
+        });
+        getPageOfMessages(r, result);
+      } else {
+        console.log('dataset complete?')
+        // begin getting messages
+        console.log(result)
+        result.filter(function(m){
+          getMessage(userId,m.id,msgHandler)
+        })
+        return result
+      }
+    });
+  };
+  var initialRequest = gmail.users.messages.list({
+    'userId': userId,
+    'q': query
+  });
+  getPageOfMessages(initialRequest, []);
+}
+
